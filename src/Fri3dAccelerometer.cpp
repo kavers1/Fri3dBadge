57baf0c36fdd3c930c0743e952ec439ccfca183e
kavers1 <koen.verstringe@scarlet.be>
2019-06-01 11:00:53 +0200

    ADXL345 added functionality to class

--- src/Fri3dAccelerometer.cpp
+++ src/Fri3dAccelerometer.cpp
@@ -14,6 +14,8 @@ Fri3dAccelerometer::begin() {
     while(1);
   }
   adxl345.setRange(ADXL345_RANGE_16_G);
+
+  clearSettings();
 }
 
 bool 
@@ -123,3 +125,392 @@ Fri3dAccelerometer::displayRange(void) {
   Serial.println(" g");  
 }
 
+void 
+Fri3dAccelerometer::clearSettings(void)
+{
+    setRange(ADXL345_RANGE_2_G);
+    setDataRate(ADXL345_DATARATE_100_HZ);
+
+    writeRegister(ADXL345_REG_THRESH_TAP, 0x00);
+    writeRegister(ADXL345_REG_DUR, 0x00);
+    writeRegister(ADXL345_REG_LATENT, 0x00);
+    writeRegister(ADXL345_REG_WINDOW, 0x00);
+    writeRegister(ADXL345_REG_THRESH_ACT, 0x00);
+    writeRegister(ADXL345_REG_THRESH_INACT, 0x00);
+    writeRegister(ADXL345_REG_TIME_INACT, 0x00);
+    writeRegister(ADXL345_REG_THRESH_FF, 0x00);
+    writeRegister(ADXL345_REG_TIME_FF, 0x00);
+
+    uint8_t value;
+
+    value = readRegister(ADXL345_REG_ACT_INACT_CTL);
+    value &= 0b10001000;
+    writeRegister(ADXL345_REG_ACT_INACT_CTL, value);
+
+    value = readRegister(ADXL345_REG_TAP_AXES);
+    value &= 0b11111000;
+    writeRegister(ADXL345_REG_TAP_AXES, value);
+}
+
+// Set Tap Threshold (62.5mg / LSB)
+void 
+Fri3dAccelerometer::setTapThreshold(float threshold)
+{
+    uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+    writeRegister(ADXL345_REG_THRESH_TAP, scaled);
+}
+
+// Get Tap Threshold (62.5mg / LSB)
+float 
+Fri3dAccelerometer::getTapThreshold(void)
+{
+    return readRegister(ADXL345_REG_THRESH_TAP) * 0.0625f;
+}
+
+// Set Tap Duration (625us / LSB)
+void 
+Fri3dAccelerometer::setTapDuration(float duration)
+{
+    uint8_t scaled = constrain(duration / 0.000625f, 0, 255);
+    writeRegister(ADXL345_REG_DUR, scaled);
+}
+
+// Get Tap Duration (625us / LSB)
+float 
+Fri3dAccelerometer::getTapDuration(void)
+{
+    return readRegister(ADXL345_REG_DUR) * 0.000625f;
+}
+
+// Set Double Tap Latency (1.25ms / LSB)
+void 
+Fri3dAccelerometer::setDoubleTapLatency(float latency)
+{
+    uint8_t scaled = constrain(latency / 0.00125f, 0, 255);
+    writeRegister(ADXL345_REG_LATENT, scaled);
+}
+
+// Get Double Tap Latency (1.25ms / LSB)
+float 
+Fri3dAccelerometer::getDoubleTapLatency()
+{
+    return readRegister(ADXL345_REG_LATENT) * 0.00125f;
+}
+
+// Set Double Tap Window (1.25ms / LSB)
+void 
+Fri3dAccelerometer::setDoubleTapWindow(float window)
+{
+    uint8_t scaled = constrain(window / 0.00125f, 0, 255);
+    writeRegister(ADXL345_REG_WINDOW, scaled);
+}
+
+// Get Double Tap Window (1.25ms / LSB)
+float 
+Fri3dAccelerometer::getDoubleTapWindow(void)
+{
+    return readRegister(ADXL345_REG_WINDOW) * 0.00125f;
+}
+
+// Set Activity Threshold (62.5mg / LSB)
+void 
+Fri3dAccelerometer::setActivityThreshold(float threshold)
+{
+    uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+    writeRegister(ADXL345_REG_THRESH_ACT, scaled);
+}
+
+// Get Activity Threshold (65.5mg / LSB)
+float 
+Fri3dAccelerometer::getActivityThreshold(void)
+{
+    return readRegister(ADXL345_REG_THRESH_ACT) * 0.0625f;
+}
+
+// Set Inactivity Threshold (65.5mg / LSB)
+void 
+Fri3dAccelerometer::setInactivityThreshold(float threshold)
+{
+    uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+    writeRegister(ADXL345_REG_THRESH_INACT, scaled);
+}
+
+// Get Incactivity Threshold (65.5mg / LSB)
+float 
+Fri3dAccelerometer::getInactivityThreshold(void)
+{
+    return readRegister(ADXL345_REG_THRESH_INACT) * 0.0625f;
+}
+
+// Set Inactivity Time (s / LSB)
+void 
+Fri3dAccelerometer::setTimeInactivity(uint8_t time)
+{
+    writeRegister(ADXL345_REG_TIME_INACT, time);
+}
+
+// Get Inactivity Time (s / LSB)
+uint8_t 
+Fri3dAccelerometer::getTimeInactivity(void)
+{
+    return readRegister(ADXL345_REG_TIME_INACT);
+}
+
+// Set Free Fall Threshold (65.5mg / LSB)
+void 
+Fri3dAccelerometer::setFreeFallThreshold(float threshold)
+{
+    uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+    writeRegister(ADXL345_REG_THRESH_FF, scaled);
+}
+
+// Get Free Fall Threshold (65.5mg / LSB)
+float 
+Fri3dAccelerometer::getFreeFallThreshold(void)
+{
+    return readRegister(ADXL345_REG_THRESH_FF) * 0.0625f;
+}
+
+// Set Free Fall Duratiom (5ms / LSB)
+void 
+Fri3dAccelerometer::setFreeFallDuration(float duration)
+{
+    uint8_t scaled = constrain(duration / 0.005f, 0, 255);
+    writeRegister(ADXL345_REG_TIME_FF, scaled);
+}
+
+// Get Free Fall Duratiom
+float 
+Fri3dAccelerometer::getFreeFallDuration()
+{
+    return readRegister(ADXL345_REG_TIME_FF) * 0.005f;
+}
+
+void 
+Fri3dAccelerometer::setActivityX(bool state)
+{
+    writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 6, state);
+}
+
+bool 
+Fri3dAccelerometer::getActivityX(void)
+{
+    return readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 6);
+}
+
+void 
+Fri3dAccelerometer::setActivityY(bool state)
+{
+    writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 5, state);
+}
+
+bool 
+Fri3dAccelerometer::getActivityY(void)
+{
+    return readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 5);
+}
+
+void 
+Fri3dAccelerometer::setActivityZ(bool state)
+{
+    writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 4, state);
+}
+
+bool 
+Fri3dAccelerometer::getActivityZ(void)
+{
+    return readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 4);
+}
+
+void 
+Fri3dAccelerometer::setActivityXYZ(bool state)
+{
+    uint8_t value;
+
+    value = readRegister(ADXL345_REG_ACT_INACT_CTL);
+
+    if (state)
+    {
+	value |= 0b00111000;
+    } else
+    {
+	value &= 0b11000111;
+    }
+
+    writeRegister(ADXL345_REG_ACT_INACT_CTL, value);
+}
+
+
+void 
+Fri3dAccelerometer::setInactivityX(bool state) 
+{
+    writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 2, state);
+}
+
+bool 
+Fri3dAccelerometer::getInactivityX(void)
+{
+    return readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 2);
+}
+
+void 
+Fri3dAccelerometer::setInactivityY(bool state)
+{
+    writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 1, state);
+}
+
+bool 
+Fri3dAccelerometer::getInactivityY(void)
+{
+    return readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 1);
+}
+
+void 
+Fri3dAccelerometer::setInactivityZ(bool state)
+{
+    writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 0, state);
+}
+
+bool 
+Fri3dAccelerometer::getInactivityZ(void)
+{
+    return readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 0);
+}
+
+void 
+Fri3dAccelerometer::setInactivityXYZ(bool state)
+{
+    uint8_t value;
+
+    value = readRegister(ADXL345_REG_ACT_INACT_CTL);
+
+    if (state)
+    {
+	value |= 0b00000111;
+    } else
+    {
+	value &= 0b11111000;
+    }
+
+    writeRegister(ADXL345_REG_ACT_INACT_CTL, value);
+}
+
+void 
+Fri3dAccelerometer::setTapDetectionX(bool state)
+{
+    writeRegisterBit(ADXL345_REG_TAP_AXES, 2, state);
+}
+
+bool 
+Fri3dAccelerometer::getTapDetectionX(void)
+{
+    return readRegisterBit(ADXL345_REG_TAP_AXES, 2);
+}
+
+void 
+Fri3dAccelerometer::setTapDetectionY(bool state)
+{
+    writeRegisterBit(ADXL345_REG_TAP_AXES, 1, state);
+}
+
+bool 
+Fri3dAccelerometer::getTapDetectionY(void)
+{
+    return readRegisterBit(ADXL345_REG_TAP_AXES, 1);
+}
+
+void 
+Fri3dAccelerometer::setTapDetectionZ(bool state)
+{
+    writeRegisterBit(ADXL345_REG_TAP_AXES, 0, state);
+}
+
+bool 
+Fri3dAccelerometer::getTapDetectionZ(void)
+{
+    return readRegisterBit(ADXL345_REG_TAP_AXES, 0);
+}
+
+void 
+Fri3dAccelerometer::setTapDetectionXYZ(bool state)
+{
+    uint8_t value;
+
+    value = readRegister(ADXL345_REG_TAP_AXES);
+
+    if (state)
+    {
+	value |= 0b00000111;
+    } else
+    {
+	value &= 0b11111000;
+    }
+
+    writeRegister(ADXL345_REG_TAP_AXES, value);
+}
+
+
+void 
+Fri3dAccelerometer::useInterrupt(adxl345_int_t interrupt)
+{
+    if (interrupt == 0)
+    {
+	writeRegister(ADXL345_REG_INT_MAP, 0x00);
+    } else
+    {
+	writeRegister(ADXL345_REG_INT_MAP, 0xFF);
+    }
+
+    writeRegister(ADXL345_REG_INT_ENABLE, 0xFF);
+}
+
+Activites 
+Fri3dAccelerometer::readActivites(void)
+{
+    uint8_t data = readRegister(ADXL345_REG_INT_SOURCE);
+
+    a.isOverrun = ((data >> ADXL345_OVERRUN) & 1);
+    a.isWatermark = ((data >> ADXL345_WATERMARK) & 1);
+    a.isFreeFall = ((data >> ADXL345_FREE_FALL) & 1);
+    a.isInactivity = ((data >> ADXL345_INACTIVITY) & 1);
+    a.isActivity = ((data >> ADXL345_ACTIVITY) & 1);
+    a.isDoubleTap = ((data >> ADXL345_DOUBLE_TAP) & 1);
+    a.isTap = ((data >> ADXL345_SINGLE_TAP) & 1);
+    a.isDataReady = ((data >> ADXL345_DATA_READY) & 1);
+
+    data = readRegister(ADXL345_REG_ACT_TAP_STATUS);
+
+    a.isActivityOnX = ((data >> 6) & 1);
+    a.isActivityOnY = ((data >> 5) & 1);
+    a.isActivityOnZ = ((data >> 4) & 1);
+    a.isTapOnX = ((data >> 2) & 1);
+    a.isTapOnY = ((data >> 1) & 1);
+    a.isTapOnZ = ((data >> 0) & 1);
+
+    return a;
+}
+
+void 
+Fri3dAccelerometer::writeRegisterBit(uint8_t reg, uint8_t pos, bool state)
+{
+    uint8_t value;
+    value = readRegister(reg);
+
+    if (state)
+    {
+	value |= (1 << pos);
+    } else 
+    {
+	value &= ~(1 << pos);
+    }
+
+    writeRegister(reg, value);
+}
+
+bool 
+Fri3dAccelerometer::readRegisterBit(uint8_t reg, uint8_t pos)
+{
+    uint8_t value;
+    value = readRegister(reg);
+    return ((value >> pos) & 1);
+}
--- src/Fri3dAccelerometer.h
+++ src/Fri3dAccelerometer.h
@@ -1,9 +1,49 @@
 // ensure this library description is only included once
+// 
+// expanded based on the ADXL library https://github.com/jarzebski/Arduino-ADXL345
+// Version: 1.1.0
+//
 #ifndef Fri3dAccelerometer_h
 #define Fri3dAccelerometer_h
 
 #include "Adafruit_ADXL345_U.h"
 #include "Adafruit_Sensor.h"
+
+typedef enum
+{
+    ADXL345_INT2 = 0b01,
+    ADXL345_INT1 = 0b00
+} adxl345_int_t;
+
+typedef enum
+{
+    ADXL345_DATA_READY         = 0x07,
+    ADXL345_SINGLE_TAP         = 0x06,
+    ADXL345_DOUBLE_TAP         = 0x05,
+    ADXL345_ACTIVITY           = 0x04,
+    ADXL345_INACTIVITY         = 0x03,
+    ADXL345_FREE_FALL          = 0x02,
+    ADXL345_WATERMARK          = 0x01,
+    ADXL345_OVERRUN            = 0x00
+} adxl345_activity_t;
+
+struct Activites
+{
+    bool isOverrun;
+    bool isWatermark;
+    bool isFreeFall;
+    bool isInactivity;
+    bool isActivity;
+    bool isActivityOnX;
+    bool isActivityOnY;
+    bool isActivityOnZ;
+    bool isDoubleTap;
+    bool isTap;
+    bool isTapOnX;
+    bool isTapOnY;
+    bool isTapOnZ;
+    bool isDataReady;
+};
 
 // library interface description
 class Fri3dAccelerometer {
@@ -19,6 +59,70 @@ public:
   void displaySensorDetails(void);
   void displayDataRate(void);
   void displayRange(void);
+
+  void clearSettings(void);
+  
+  Activites readActivites(void);
+
+  Vector lowPassFilter(Vector vector, float alpha = 0.5);
+
+  void setTapThreshold(float threshold);
+  float getTapThreshold(void);
+
+  void setTapDuration(float duration);
+  float getTapDuration(void);
+
+  void setDoubleTapLatency(float latency);
+  float getDoubleTapLatency(void);
+
+  void setDoubleTapWindow(float window);
+  float getDoubleTapWindow(void);
+
+  void setActivityThreshold(float threshold);
+  float getActivityThreshold(void);
+
+  void setInactivityThreshold(float threshold);
+  float getInactivityThreshold(void);
+
+  void setTimeInactivity(uint8_t time);
+  uint8_t getTimeInactivity(void);
+
+  void setFreeFallThreshold(float threshold);
+  float getFreeFallThreshold(void);
+
+  void setFreeFallDuration(float duration);
+  float getFreeFallDuration();
+
+  void setActivityX(bool state);
+  bool getActivityX(void);
+  void setActivityY(bool state);
+  bool getActivityY(void);
+  void setActivityZ(bool state);
+  bool getActivityZ(void);
+  void setActivityXYZ(bool state);
+
+  void setInactivityX(bool state);
+  bool getInactivityX(void);
+  void setInactivityY(bool state);
+  bool getInactivityY(void);
+  void setInactivityZ(bool state);
+  bool getInactivityZ(void);
+  void setInactivityXYZ(bool state);
+
+  void setTapDetectionX(bool state);
+  bool getTapDetectionX(void);
+  void setTapDetectionY(bool state);
+  bool getTapDetectionY(void);
+  void setTapDetectionZ(bool state);
+  bool getTapDetectionZ(void);
+  void setTapDetectionXYZ(bool state);
+
+  void useInterrupt(adxl345_int_t interrupt);
+
+  private:
+  Activites a;
+  range_t _range;
+
 };
 
 #endif
